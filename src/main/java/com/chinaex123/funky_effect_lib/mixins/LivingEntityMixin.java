fa303a220d6b2f9e21174fc5c0cb638615e4a75d
WitherRedstone <1751216077@qq.com>
2026-07-07 21:40:01 +0800

    1.0.3

--- src/main/java/com/chinaex123/funky_effect_lib/mixins/LivingEntityMixin.java
+++ src/main/java/com/chinaex123/funky_effect_lib/mixins/LivingEntityMixin.java
@@ -11,17 +11,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
-    /** 棷入方法，用于修改实体是否发光 **/
+    /** 注入方法，用于修改实体是否发光 **/
     @Inject(method = "isCurrentlyGlowing", at = @At("RETURN"), cancellable = true)
     private void onIsCurrentlyGlowing(CallbackInfoReturnable<Boolean> cir) {
         LivingEntity entity = (LivingEntity)(Object)this;
 
-        boolean shouldGlow = false;
-
-        // 如果 DangerSense 效果激活且实体在列表中，返回 true
-        if (DangerSenseRender.isEffectActive() && DangerSenseRender.shouldEntityGlow(entity)) {
-            shouldGlow = true;
-        }
+        // 如果 DangerSense 效果激活且实体在列表中，或者原版已经返回 true，则返回 true
+        boolean shouldGlow = cir.getReturnValue() || (DangerSenseRender.isEffectActive() && DangerSenseRender.shouldEntityGlow(entity));
 
         cir.setReturnValue(shouldGlow);
     }
