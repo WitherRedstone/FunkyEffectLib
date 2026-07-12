0dd6a07421e8ac1a7ec825ac1a1c78163e85912e
WitherRedstone <1751216077@qq.com>
2026-07-12 12:42:31 +0800

    1.1.1

--- src/main/java/com/chinaex123/funky_effect_lib/api/BoltChargeAPI.java
+++ src/main/java/com/chinaex123/funky_effect_lib/api/BoltChargeAPI.java
@@ -119,9 +119,10 @@ public class BoltChargeAPI {
                     SoundSource.MASTER, 1.0f, 1.0f);
         }
 
+        int previousCount = getChargeCount(attacker);
         clearCharges(attacker);
         BoltCharge.syncToClient(attacker, 0);
         
-        MinecraftForge.EVENT_BUS.post(new BoltChargeDischargedEvent(attacker, MAX_CHARGES, target, lightning));
+        MinecraftForge.EVENT_BUS.post(new BoltChargeDischargedEvent(attacker, MAX_CHARGES, target, lightning, previousCount));
     }
 }
