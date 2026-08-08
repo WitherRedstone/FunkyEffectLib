2efc498deb1e21b3e0e64c4a3b7cf93ccd9906c9
WitherRedstone <1751216077@qq.com>
2026-08-08 10:46:41 +0800

    1.1.3-更改“灼烧”叠层

--- src/main/java/com/chinaex123/funky_effect_lib/effect/Scorch.java
+++ src/main/java/com/chinaex123/funky_effect_lib/effect/Scorch.java
@@ -39,8 +39,6 @@ public class Scorch extends MobEffect {
 
     /** 叠层间隔 **/
     private static final int TICKS_PER_STACK = 20;
-    /** 灼烧最高层数 **/
-    private static final int MAX_SCORCH_STACKS = 100;
     /** 100层后触发间隔 **/
     private static final int POST_MAX_TICKS = 100;
     /** 保持燃烧状态的时间 **/
@@ -72,7 +70,7 @@ public class Scorch extends MobEffect {
             int currentStacks = ScorchAPI.getScorchStacks(entity);
 
             // 如果已经达到最高层数，进入特殊模式
-            if (currentStacks >= MAX_SCORCH_STACKS) {
+            if (currentStacks >= ScorchAPI.MAX_SCORCH_STACKS) {
                 handlePostMaxMode(entity, entityId);
                 return true;
             }
@@ -91,7 +89,7 @@ public class Scorch extends MobEffect {
                 syncToClient(entity, newStacks);
 
                 // 检查是否刚刚达到100层
-                if (newStacks == MAX_SCORCH_STACKS) {
+                if (newStacks == ScorchAPI.MAX_SCORCH_STACKS) {
                     onReachMaxStacks(entity);
                 }
             } else {
